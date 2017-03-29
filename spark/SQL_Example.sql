9ee991be411446a5647a6ac7df7a4a9fc61da077
ng310285 <Nathan.Green@teradata.com>
2017-03-29 16:16:17 +1100

    Update SQL_Example.sql

--- spark/SQL_Example.sql
+++ spark/SQL_Example.sql
@@ -1,25 +1,34 @@
+CREATE DATABASE spark FROM DBC AS perm=1e9;
+
 DATABASE spark;
 GRANT ALL ON spark TO dbc;
--- initial installation of script
-CALL sysuif.install_file('spark_script', 'spark_script.py', 'sz!/root/spark/spark_script.py');
--- to replace/update script
+
 CALL sysuif.replace_file('spark_script', 'spark_script.py', 'sz!/root/spark/spark_script.py', 0);
+
+SHOW FILE spark.spark_script;
 
 SET SESSION SEARCHUIFDBPATH = spark;
 
-CREATE TABLE spark.query_ngrams AS (
-   SELECT    tab.QueryID
-           , CAST(tab.json_text AS JSON) AS ngrams
-FROM SCRIPT (
-    ON (
-            SELECT   QueryId
-	                 , SqlTextInfo
-		        FROM DBC.QryLogSQL
-			) PARTITION BY QueryID 
-	SCRIPT_COMMAND('source /etc/profile;ulimit -v 1000000;env > /tmp/env_$$.txt;/usr/local/bin/python3 ./spark/spark_script.py')
-	RETURNS ('QueryID VARCHAR(30)',  'json_text CLOB')
-   ) AS tab
-) WITH DATA
-PRIMARY INDEX (QueryID)
+drop table  spark.query_ngrams;
+
+    CREATE TABLE spark.query_ngrams AS (
+    SELECT    tab.QueryID
+                      , CAST(tab.json_text AS JSON) AS ngrams
+    FROM SCRIPT (
+        ON (
+                    SELECT    QueryId
+                                     , SqlTextInfo
+                    FROM DBC.QryLogSQL
+                ) PARTITION BY QueryID 
+        SCRIPT_COMMAND('sh --login -c "python ./spark/spark_script.py"')
+        RETURNS ('QueryID VARCHAR(30)',  'json_text CLOB')
+    ) AS tab
+    ) WITH DATA
+    PRIMARY INDEX (QueryID)
+    ;
+                            
+SELECT COUNT(ngrams)
+FROM spark.query_ngrams
 ;
+
 				
