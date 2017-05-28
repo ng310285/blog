e4678e13c7a7f593a813347fddee13d4f3f59d85
Nathan Green <Nathan.Green@teradata.com>
2017-05-29 07:36:55 +1000

    cleaned up a little

--- spark/aws_commands.sh
+++ spark/aws_commands.sh
@@ -1,12 +1,13 @@
 # Commands to get the spark cluster up and running on AWS
 # Using an AWS Linux instance (large or larger, to get enough memory)
+#sudo yum install git
+
 sudo yum update
 sudo yum install docker
 sudo pip install docker-compose
-sudo yum install git
 sudo service docker start
 
-git clone https://github.com/ng310285/blog.git
-cd blog/spark
-sudo docker build -t spark-2:latest .
-sudo /usr/local/bin/docker-compose up
+#git clone https://github.com/ng310285/blog.git
+#cd blog/spark
+#sudo docker build -t spark-2:latest .
+#sudo /usr/local/bin/docker-compose up
